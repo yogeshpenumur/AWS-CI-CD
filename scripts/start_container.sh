1a939a01624f5e815733f89f23f8e2735a37404a
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-06-06 17:44:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 # Pull the Docker image from Docker Hub
-docker pull yogeshpenumur/flask-app
+docker pull yogeshpenumur/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 yogeshpenumur/flask-app
+docker run -d -p 5000:5000 yogeshpenumur/simple-python-flask-app:latest
 
