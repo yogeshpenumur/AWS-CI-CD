06b187582c7d4544b6bd0c69c66b717e207df1f9
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-06-06 20:23:10 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Define the container name
-#CONTAINER_NAME="myapp"
+CONTAINER_NAME="myapp"
 
 # Check if the container is running
-#if docker ps -q -f name="$CONTAINER_NAME" | grep -q .; then
-  #echo "Stopping and removing running container: $CONTAINER_NAME"
-  #docker rm -f "$CONTAINER_NAME"
-#else
-  #echo "No running container named $CONTAINER_NAME found."
-#fi
+if docker ps -q -f name="$CONTAINER_NAME" | grep -q .; then
+  echo "Stopping and removing running container: $CONTAINER_NAME"
+  docker rm -f "$CONTAINER_NAME"
+else
+  echo "No running container named $CONTAINER_NAME found."
+fi
