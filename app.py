6949494da554253065e12e7884d6ba1176aff180
yogesh <117835365+yogeshpenumur@users.noreply.github.com>
2025-06-07 11:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def logout():
 if __name__ == "__main__":
     if not os.path.exists("db.sqlite3"):
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=9111)
+    app.run(debug=True,host='0.0.0.0'port=9111)
