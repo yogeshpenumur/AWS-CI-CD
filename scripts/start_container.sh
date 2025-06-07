45372d88c2e110b9f9808359289634da3d09b3cf
yogesh <yogeshpenumur2002@gmail.com>
2025-06-07 09:22:20 +0530

    1-v

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,13 +3,7 @@ set -e
 
 IMAGE_NAME="yogeshpenumur/simple-python-flask-app"
 CONTAINER_NAME="flask-app"
-<<<<<<< HEAD
 PORT=9111
-=======
-
-PORT=9111
-
->>>>>>> 8267dbe165a8f589c18096118271daeffa5eb5eb
 
 # Stop and remove old container using the same name (if exists)
 if docker ps -a --format '{{.Names}}' | grep -w "$CONTAINER_NAME" > /dev/null; then
@@ -32,10 +26,4 @@ docker pull "$IMAGE_NAME"
 
 # Run the new container
 echo "Starting new container: $CONTAINER_NAME"  
-<<<<<<< HEAD
 docker run -d --name "$CONTAINER_NAME" -p $PORT:9111 "$IMAGE_NAME"
-=======
-
-docker run -d --name "$CONTAINER_NAME" -p $PORT:9111 "$IMAGE_NAME"
-
->>>>>>> 8267dbe165a8f589c18096118271daeffa5eb5eb
